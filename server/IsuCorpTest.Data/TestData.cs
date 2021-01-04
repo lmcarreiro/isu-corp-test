ed61c4c57b7900de055a2de9daac52466379fb72
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2021-01-04 19:03:50 -0400

    Fix build error

--- server/IsuCorpTest.Data/TestData.cs
+++ server/IsuCorpTest.Data/TestData.cs
@@ -16,7 +16,7 @@ namespace IsuCorpTest.Data
         {
             Context = context;
         }
-
+        
         public void InsertData()
         {
             // Creates the database if not exists
@@ -26,33 +26,33 @@ namespace IsuCorpTest.Data
 
             ContactType[] contactTypes = new[]
             {
-                new ContactType("Contact Type 1"),
-                new ContactType("Contact Type 2"),
-                new ContactType("Contact Type 3"),
+                new ContactType { Name = "Contact Type 1" },
+                new ContactType { Name = "Contact Type 2" },
+                new ContactType { Name = "Contact Type 3" },
             };
             Context.ContactType.AddRange(contactTypes);
 
             Contact[] contacts = new[]
             {
-                new Contact("Second Dock"  , "+55 21 987-654-321") { BirthDate = new DateTime(2001, 12,  1), Type = contactTypes[0] },
-                new Contact("Primer Puerto", "+55 21 987-654-321") { BirthDate = new DateTime(2002, 10,  5), Type = contactTypes[0] },
-                new Contact("Stella"       , "+55 21 987-654-321") { BirthDate = new DateTime(2003,  8, 10), Type = contactTypes[1] },
-                new Contact("Island Creek" , "+55 21 987-654-321") { BirthDate = new DateTime(2004,  6, 15), Type = contactTypes[1] },
-                new Contact("Fogo the Chao", "+55 21 987-654-321") { BirthDate = new DateTime(2005,  4, 20), Type = contactTypes[2] },
-                new Contact("Fontana"      , "+55 21 987-654-321") { BirthDate = new DateTime(2006,  2, 25), Type = contactTypes[2] },
+                new Contact { Name = "Second Dock"  , Phone = "+55 21 987-654-321", BirthDate = new DateTime(2001, 12,  1), Type = contactTypes[0] },
+                new Contact { Name = "Primer Puerto", Phone = "+55 21 987-654-321", BirthDate = new DateTime(2002, 10,  5), Type = contactTypes[0] },
+                new Contact { Name = "Stella"       , Phone = "+55 21 987-654-321", BirthDate = new DateTime(2003,  8, 10), Type = contactTypes[1] },
+                new Contact { Name = "Island Creek" , Phone = "+55 21 987-654-321", BirthDate = new DateTime(2004,  6, 15), Type = contactTypes[1] },
+                new Contact { Name = "Fogo the Chao", Phone = "+55 21 987-654-321", BirthDate = new DateTime(2005,  4, 20), Type = contactTypes[2] },
+                new Contact { Name = "Fontana"      , Phone = "+55 21 987-654-321", BirthDate = new DateTime(2006,  2, 25), Type = contactTypes[2] },
             };
             Context.Contact.AddRange(contacts);
 
             Reservation[] reservations = new[]
             {
-                new Reservation("Test number <b>1</b>") { Contact = contacts[0], DateTime = new DateTime(2020, 5, 17, 21, 0, 0), Favorite = true , Ranking = 4    },
-                new Reservation("Test number <b>2</b>") { Contact = contacts[1], DateTime = new DateTime(2020, 5, 18, 20, 0, 0), Favorite = false, Ranking = 3    },
-                new Reservation("Test number <b>3</b>") { Contact = contacts[2], DateTime = new DateTime(2020, 5, 20, 19, 0, 0), Favorite = false, Ranking = 2    },
-                new Reservation("Test number <b>4</b>") { Contact = contacts[3], DateTime = new DateTime(2020, 5, 21, 20, 0, 0), Favorite = false, Ranking = 2    },
-                new Reservation("Test number <b>5</b>") { Contact = contacts[4], DateTime = new DateTime(2020, 5, 17, 21, 0, 0), Favorite = true , Ranking = 2    },
-                new Reservation("Test number <b>6</b>") { Contact = contacts[5], DateTime = new DateTime(2020, 5, 23, 20, 0, 0), Favorite = false, Ranking = 2    },
-                new Reservation("Test number <b>7</b>") { Contact = contacts[3], DateTime = new DateTime(2020, 5, 24, 21, 0, 0), Favorite = true , Ranking = 1    },
-                new Reservation("Test number <b>8</b>") { Contact = contacts[4], DateTime = new DateTime(2020, 5, 25, 20, 0, 0), Favorite = false, Ranking = null },
+                new Reservation { Contact = contacts[0], DateTime = new DateTime(2020, 5, 17, 21, 0, 0), Favorite = true , Ranking = 4   , Description = "Test number <b>1</b>" },
+                new Reservation { Contact = contacts[1], DateTime = new DateTime(2020, 5, 18, 20, 0, 0), Favorite = false, Ranking = 3   , Description = "Test number <b>2</b>" },
+                new Reservation { Contact = contacts[2], DateTime = new DateTime(2020, 5, 20, 19, 0, 0), Favorite = false, Ranking = 2   , Description = "Test number <b>3</b>" },
+                new Reservation { Contact = contacts[3], DateTime = new DateTime(2020, 5, 21, 20, 0, 0), Favorite = false, Ranking = 2   , Description = "Test number <b>4</b>" },
+                new Reservation { Contact = contacts[4], DateTime = new DateTime(2020, 5, 17, 21, 0, 0), Favorite = true , Ranking = 2   , Description = "Test number <b>5</b>" },
+                new Reservation { Contact = contacts[5], DateTime = new DateTime(2020, 5, 23, 20, 0, 0), Favorite = false, Ranking = 2   , Description = "Test number <b>6</b>" },
+                new Reservation { Contact = contacts[3], DateTime = new DateTime(2020, 5, 24, 21, 0, 0), Favorite = true , Ranking = 1   , Description = "Test number <b>7</b>" },
+                new Reservation { Contact = contacts[4], DateTime = new DateTime(2020, 5, 25, 20, 0, 0), Favorite = false, Ranking = null, Description = "Test number <b>8</b>" },
             };
             Context.Reservation.AddRange(reservations);
 
