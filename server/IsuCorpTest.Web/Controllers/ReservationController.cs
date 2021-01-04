a024cb74ce20bbe0335bd5c64c2c349aba7a5c4c
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2021-01-03 20:55:09 -0400

    Use of a custom base controller

---
+++ server/IsuCorpTest.Web/Controllers/ReservationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IsuCorpTest.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IsuCorpTest.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReservationController : CustomControllerBase
+    {
+        public ReservationController(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        [HttpGet]
+        public async Task<ReservationListItem[]> Get()
+        {
+            var reservations = await UnitOfWork.Reservation.ListAll();
+            return reservations
+                .Select(r => new ReservationListItem(
+                    r.Id,
+                    r.Contact.Name,
+                    r.DateTime,
+                    r.Ranking ?? 0,
+                    r.Favorite
+                ))
+                .ToArray();
+        }
+    }
+
+    public record ReservationListItem(int Id, string ContactName, DateTime ReservationDate, int Ranking, bool Favorite);
+}
---
+++ server/IsuCorpTest.Web/CustomControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using IsuCorpTest.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsuCorpTest.Web
+{
+    public abstract class CustomControllerBase : ControllerBase
+    {
+        protected IUnitOfWork UnitOfWork { get; }
+
+        public CustomControllerBase(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+    }
+}
